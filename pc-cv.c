6b8c00da8053eff51df5085e5382a7559987080f
jmu201621143028 <2216164102@qq.com>
2022-08-20 20:28:42 +0800

    多个生产者和消费者
    1.if更换为while是为了保证：唤醒后，如果条件不满足则继续进入wait。
    2.pthread_cond_signal更换为pthread_cond_broadcast是为了避免死锁。
      (存在一种情况，消费者之间相互唤醒，生产者wait，进入死锁状态)

--- pc-cv.c
+++ pc-cv.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <unistd.h>
 
 
 pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
@@ -13,14 +14,14 @@ void TProduce()
     while (1)
     {
         pthread_mutex_lock(&mutex1);
-        if(n == count)
+        while(n == count)
         {
             // pthread_mutex_unlock(&mutex1);
             pthread_cond_wait(&condition_cond, &mutex1);
         }
         count++;
         printf("(");
-        pthread_cond_signal(&condition_cond);
+        pthread_cond_broadcast(&condition_cond);
         pthread_mutex_unlock(&mutex1);
     }
 }
@@ -30,14 +31,14 @@ void TConsume()
     while (1)
     {
         pthread_mutex_lock(&mutex1);
-        if(0 == count)
+        while(0 == count)
         {
             //pthread_mutex_unlock(&mutex1);
             pthread_cond_wait(&condition_cond, &mutex1);
         }
         count--;
         printf(")");
-        pthread_cond_signal(&condition_cond);
+        pthread_cond_broadcast(&condition_cond);
         pthread_mutex_unlock(&mutex1);
     }
 }
@@ -51,9 +52,17 @@ int main(int args, char * argv[])
     char *message1 = "Thread 1";
     char *message2 = "Thread 2";
     int iret1, iret2;
-    iret1 = pthread_create(&thread1, NULL, (void *)TProduce, (void *)message1);
-    iret2 = pthread_create(&thread2, NULL, (void *)TConsume, (void *)message2);
-    while (1); // 
+    for(int i = 0; i < 8; ++i){
+        iret1 = pthread_create(&thread1, NULL, (void *)TProduce, (void *)message1);
+        iret2 = pthread_create(&thread2, NULL, (void *)TConsume, (void *)message2);
+    }
+    //iret1 = pthread_create(&thread1, NULL, (void *)TProduce, (void *)message1);
+    //iret2 = pthread_create(&thread2, NULL, (void *)TConsume, (void *)message2);
+    //iret2 = pthread_create(&thread2, NULL, (void *)TConsume, (void *)message2);
+
+    while (1){
+        sleep(1);
+    }; // 
     
     return 0;
 }
